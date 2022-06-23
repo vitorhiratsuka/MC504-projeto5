3c0071b44cc6a49364beac1da8477b4773d0fdc8
Thiago Donato Ferreira <flowlnlnln@gmail.com>
2022-06-23 20:15:00 -0300

    feat: add get/set user weight syscalls to unistd.h

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -886,8 +886,13 @@ __SYSCALL(__NR_futex_waitv, sys_futex_waitv)
 #define __NR_set_mempolicy_home_node 450
 __SYSCALL(__NR_set_mempolicy_home_node, sys_set_mempolicy_home_node)
 
+#define __NR_getuserweight 451
+__SYSCALL(__NR_getuserweight, sys_getuserweight)
+#define __NR_setuserweight 452
+__SYSCALL(__NR_setuserweight, sys_setuserweight)
+
 #undef __NR_syscalls
-#define __NR_syscalls 451
+#define __NR_syscalls 453
 
 /*
  * 32 bit systems traditionally used different
